fc1a8954ef9112adcbb63c981c04dfd0e559010f
7mon <simon.boutrouille123@gmail.com>
2025-08-05 23:33:15 +0200

    Update checkmark theme

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -1,8 +1,14 @@
 import styles from "./SkillsStyles.module.css";
-import checkMarkIcon from "../../assets/checkmark-dark.svg";
+import checkMarkDakrk from "../../assets/checkmark-dark.svg";
+import checkMarkLight from "../../assets/checkmark-light.svg";
 import SkillList from "../../common/SkillList";
+import { useTheme } from "../../common/ThemeContext.jsx";
 
 function Skills() {
+  const { theme, toggleTheme } = useTheme();
+
+  const checkMarkIcon = theme === "light" ? checkMarkLight : checkMarkDakrk;
+
   return (
     <section id="skills" className={styles.container}>
       <h1 className="sectionTitle">Skills</h1>
@@ -14,7 +20,7 @@ function Skills() {
         <SkillList src={checkMarkIcon} skill="Java" />
         <SkillList src={checkMarkIcon} skill="SQL" />
       </div>
-      <hr/>
+      <hr />
       <div className={styles.skillList}>
         <SkillList src={checkMarkIcon} skill="React" />
         <SkillList src={checkMarkIcon} skill="Angular" />
