a20c61e225034a0c2d4e73a9e1ade183afe2c131
7mon <simon.boutrouille123@gmail.com>
2025-08-05 22:27:52 +0200

    Rename CV file name

--- src/sections/Me/Me.jsx
+++ src/sections/Me/Me.jsx
@@ -7,7 +7,7 @@ import linkedinLight from "../../assets/linkedin-light.svg";
 import linkedinDark from "../../assets/linkedin-dark.svg";
 import githubLight from "../../assets/github-light.svg";
 import githubDark from "../../assets/github-dark.svg";
-import CV from "../../assets/CV.pdf";
+import CV from "../../assets/CV-Simon-Boutrouille.pdf";
 import { useTheme } from "../../common/ThemeContext.jsx";
 
 function Me() {
