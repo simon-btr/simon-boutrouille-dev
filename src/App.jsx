36cde8d72d65a1c49129a1dc4fea90db497a7c32
7mon <simon.boutrouille123@gmail.com>
2025-08-05 22:18:20 +0200

    Fix skills import in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import Me from "./sections/Me/Me.jsx";
 import Projects from "./sections/Projects/Projects.jsx";
-import Skills from "./sections/Skills/skills.jsx";
+import Skills from "./sections/Skills/Skills.jsx";
 import Contact from "./sections/Contact/Contact.jsx";
 import Footer from "./sections/Footer/Footer.jsx";
 
